00e9f6aff5a1347c0c25373d60119da7eb189380
S.P. Mohanty <spmohanty91@gmail.com>
2016-11-19 22:04:02 +0100

    Add serve, hole, turn, flop and river functions

--- deck.py
+++ deck.py
@@ -7,9 +7,16 @@ import itertools
 class Deck:
     self.cards = [str(x) for x in range(1, 10)] + ["J", "Q", "K"]
     self.suits = ["c", "d", "h", "s"]
-    def __init__():
+
+    def reset():
+        """
+            Resets the deck
+        """
         self.deck = ["".join(x) for x in list(itertools.product(cards, suits))]
 
+
+    def __init__():
+        reset()
 
     @property
     def size():
@@ -39,3 +46,30 @@ class Deck:
             _serve = self.deck[:number_of_cards]
             self.deck = self.deck[number_of_cards:]
             return _serve
+
+    def serve_hole():
+        """
+            Serve "hole"(s); two cards at the beginning of the play
+        """
+        return serve(2)
+
+    def serve_flop():
+        """
+            Serve "flops"(s); three cards placed face-up at
+            the beginning of the game
+        """
+        return serve(3)
+
+    def serve_turn():
+        """
+            Serve "turn street" or "fourth street"; the additional single card
+            made public after the flop
+        """
+        return serve(1)
+
+    def serve_river():
+        """
+            Serve "river" or "fifth street"; the final single card
+            made public after the fourth street
+        """
+        return serve(1)
